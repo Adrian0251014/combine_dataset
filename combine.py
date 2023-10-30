df4224f1f518c00736f999dd348f4f68be6b02de
Silin Chen <143574213+Adrian0251014@users.noreply.github.com>
2023-10-30 16:06:38 -0400

    Update combine.py

--- combine.py
+++ combine.py
@@ -43,36 +43,36 @@ def play_gif(gif_path):
     cv.destroyAllWindows()
 
 
-def play_mp4(mp4_path):
-    # Open the mp4 file
-    cap = cv.VideoCapture(mp4_path)
+# def play_mp4(mp4_path):
+#     # Open the mp4 file
+#     cap = cv.VideoCapture(mp4_path)
 
-    # Get the dimensions of the mp4
-    width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
-    height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+#     # Get the dimensions of the mp4
+#     width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+#     height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
 
-    # Set the window size and position
-    cv.namedWindow('mp4', cv.WINDOW_NORMAL)
-    cv.resizeWindow('mp4', width, height)
-    cv.moveWindow('mp4', 100, 100)
+#     # Set the window size and position
+#     cv.namedWindow('mp4', cv.WINDOW_NORMAL)
+#     cv.resizeWindow('mp4', width, height)
+#     cv.moveWindow('mp4', 100, 100)
 
-    # Play the video
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            print("Can't receive frame")
-            break
+#     # Play the video
+#     while cap.isOpened():
+#         ret, frame = cap.read()
+#         if not ret:
+#             print("Can't receive frame")
+#             break
 
-        # Show the current frame
-        cv.imshow('mp4', frame)
+#         # Show the current frame
+#         cv.imshow('mp4', frame)
 
-        # Wait for a while
-        key = cv.waitKey(30)  # Adjust the delay as per your video's frame rate
-        if key & 0xFF == ord(' '):
-            break
+#         # Wait for a while
+#         key = cv.waitKey(30)  # Adjust the delay as per your video's frame rate
+#         if key & 0xFF == ord(' '):
+#             break
 
-    # Close the window
-    cv.destroyAllWindows()
+#     # Close the window
+#     cv.destroyAllWindows()
 
 
 def search_and_play(file_name):
@@ -82,14 +82,14 @@ def search_and_play(file_name):
 
     # Build the file paths
     gif_path = os.path.join(gif_folder_path, file_name + '.gif')
-    mp4_path = os.path.join(mp4_folder_path, file_name + '.mp4')
+    mp4_path = os.path.join(mp4_folder_path, file_name + '.gif')
 
     # Search for GIF
     if os.path.isfile(gif_path):
         play_gif(gif_path)
     # If GIF not found, search for mp4
     elif os.path.isfile(mp4_path):
-        play_mp4(mp4_path)
+        play_gif(mp4_path)
     else:
         print("File not found")
 
